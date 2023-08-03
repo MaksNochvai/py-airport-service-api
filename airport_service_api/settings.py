8a45651333179a7ba88f5296171a5a635a0fced7
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-08-03 15:59:36 +0300

    Updated settings.py

--- airport_service_api/settings.py
+++ airport_service_api/settings.py
@@ -151,8 +151,8 @@ REST_FRAMEWORK = {
 }
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "Cinema Service API",
-    "DESCRIPTION": "Order cinema tickets",
+    "TITLE": "Airports Service API",
+    "DESCRIPTION": "Order aiprorts tickets",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
     "SWAGGER_UI_SETTINGS": {
